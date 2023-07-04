ce20ec0c2fb8a9c1792783092169eaf617ff883b
przygoda <schmidt.multimedia@googlemail.com>
2023-07-04 09:15:11 +0000

    Bedingtes Anlegen/Löschen von DBs

---
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -0,0 +1,18 @@
+/* ------  Strukturen ----- */
+
+/* Kommentar 1 (Block) */
+# Zeilenkommentar 1
+-- Zeilenkommentar 1
+
+/* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+DROP DATABASE IF EXISTS boo;
+
+SHOW DATABASES;
+
